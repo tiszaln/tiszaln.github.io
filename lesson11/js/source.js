68f192f38cfe559bde58d0298c0ec38fab1bdb64
Daniel Olsen <idanielolsen@gmail.com>
2020-06-28 15:13:25 -0600

    Include all neccessary files into lesson 11

---
+++ lesson11/js/source.js
@@ -0,0 +1,25 @@
+var today = new Date();
+var year = today.getFullYear();
+
+var options = { weekday: 'long', day: 'numeric', month: 'long',  year: 'numeric'};
+
+var current = today.toLocaleDateString('en-UK', options)
+
+console.log(current);
+
+function copyYear() {
+/*     document.getElementById("copyright").innerHTML = year; */
+    document.getElementById("today").innerHTML = current;
+};
+
+function responsiveMenu() {
+    var menu = document.getElementById("toggleMenu");
+    if (menu.innerHTML === "\u2630 Menu"){
+        menu.innerHTML = "&#10803;";
+        document.getElementById("menu").classList.toggle("hide");
+    } else {
+        menu.innerHTML = "&#9776; Menu";
+        document.getElementById("menu").classList.toggle("hide");
+    };
+};
+
